81de30449c7d1cfdc52a200bc8b43defaccccc37
Stefan Stax Miljkovic <staxstefan@gmail.com>
2021-07-13 20:05:19 +0200

    Adding service-worker

--- src/index.js
+++ src/index.js
@@ -9,14 +9,18 @@ import App from "./containers/App"
 import reportWebVitals from "./reportWebVitals";
 import { searchColleagues, requestColleagues } from "./reducers";
 import thunkMiddleware from "redux-thunk";
-import * as serviceWorker from "./serviceWorker"
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const rootReducer = combineReducers({searchColleagues, requestColleagues});
 const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById("root"));
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-serviceWorker.register();
 reportWebVitals();
