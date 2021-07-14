a391919570c39053cb0469403974f4aa1164a204
Stefan Stax Miljkovic <staxstefan@gmail.com>
2021-07-14 18:52:29 +0200

    changes for favicon | minor styling updates | screen-reader update

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = (props) => {
     return (
         <div className="text-xl text-white font-thin mx-3 my-3 p-3 rounded-md shadow-lg w-full md:w-2/6 lg:w-2/6 xl:w-1/6 text-center" style={{backgroundColor: "#004AAD"}}>
-            <img className="my-3 mx-auto rounded-full" style={{width: "70%"}} src={`https://robohash.org/${props.username}?set=set5`} alt="robots" />
+            <img className="my-3 mx-auto rounded-full" src={`https://robohash.org/${props.username}?set=set5`} width="200px" height="200px" alt="robots" />
             <h2 className="font-semibold text-black my-2 rounded-md" style={{backgroundColor: "#F5BD02"}}>{props.name}</h2>
             <p>Known By: {props.username}</p>
             <p>Email: {props.email}</p>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,6 +3,7 @@ import React from "react";
 const SearchBox = ({ searchChange }) => {
   return (
     <input
+      aria-label="Search for a colleague"
       className="w-11/12 md:w-6/12 lg:w-4/12 xl:w-2/12 mx-auto mt-7 mb-5 p-5 rounded-md"
       style={{ backgroundColor: "#F5BD02", color: "rgb(0, 74, 173)" }}
       onChange={searchChange}
